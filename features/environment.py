76cb2d4dd99d1b9dfd01ed8e4d8a2911147d1e7f
Rafael Guterres Jeffman <rafasgj@gmail.com>
2019-08-29 23:08:52 -0300

    Behave's test environment configuration.

---
+++ features/environment.py
@@ -0,0 +1,29 @@
+"""Set up testing environment."""
+
+import os
+import tempfile
+from behave import fixture, use_fixture
+
+from api import api
+
+
+@fixture
+def api_client(context, *args, **kwargs):
+    """Create an API testing client."""
+    context.db, api.config['DATABASE'] = tempfile.mkstemp()
+    api.testing = True
+    context.client = api.test_client()
+    yield context.client
+    # -- CLEANUP:
+    os.close(context.db)
+    os.unlink(api.config['DATABASE'])
+
+
+def before_scenario(context, scenario):
+    """Prepare context for testing scenario."""
+    use_fixture(api_client, context)
+
+
+def before_feature(context, feature):
+    """Prepare context for testing feature."""
+    pass
